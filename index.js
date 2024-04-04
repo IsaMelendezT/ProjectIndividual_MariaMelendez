4ed3b2bf1ecdeccd7bb882d8e6fab7e2a6450084
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-04-03 21:18:11 -0400

    Initial files

---
+++ index.js
@@ -0,0 +1,62 @@
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
+
+/* 
+exercise - complete the project based on the following instructions
+part 1
+1. add event listener for the button to add todos
+2. add event listener for the search input to filter todos
+3. add event listener for the checkbox to filter todos based on their completions
+*/
+
+/* Part 2
+1. replace paragraphs in todos with a checkbox input and create a checknox for each todo item
+2. if the todo is complete the item should be checked and there should be strike through for the item 
+3. add event listener for the checkbox for every item
+*/
